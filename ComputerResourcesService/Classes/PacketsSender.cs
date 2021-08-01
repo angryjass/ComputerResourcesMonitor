cb8c99e4a2f6f66cd42b181de3d8bb356d2a4488
Игорь Мокроусов <imokrousov@digimatics.ru>
2021-08-02 01:53:54 +1000

    second commit

---
+++ ComputerResourcesService/Classes/PacketsSender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+
+namespace ComputerResourcesService.Classes
+{
+    public static class PacketsSender
+    {
+        public static void Send(Packet packet)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpContent httpContent = new StringContent(packet.ToString());
+
+            httpClient.PostAsync(ConfigurationManager.ConnectionStrings["API"].ConnectionString, httpContent);
+        }
+    }
+}
---
+++ ComputerResourcesService/Program.cs
@@ -0,0 +1,48 @@
+﻿using ComputerResourcesService.Classes;
+using LibreHardwareMonitor.Hardware;
+using System;
+using System.Linq;
+using System.Threading;
+
+namespace ComputerResourcesService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer computer = new Computer
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = false,
+                IsMemoryEnabled = false,
+                IsMotherboardEnabled = false,
+                IsControllerEnabled = false,
+                IsNetworkEnabled = false,
+                IsStorageEnabled = false
+            };
+
+            computer.Open();
+            computer.Accept(new UpdateVisitor());
+            try
+            {
+                var cpu_total_sensor = computer.Hardware[0].Sensors.First(s => s.Name == "CPU Total");
+                while (true)
+                {
+                    Console.WriteLine($"Загрузка процессора: {cpu_total_sensor.Value}");
+
+                    Packet packet = new Packet(cpu_total_sensor.Value.ToString(), null, null, null);
+
+                    PacketsSender.Send(packet);
+                    cpu_total_sensor.Hardware.Update();
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                }
+            }
+            finally
+            {
+                computer.Close();
+            }
+
+        }
+    }
+}
